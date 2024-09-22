ead9e97e3f97620a8bbd481e85132df72b8ff938
maalik4567 <abmaalik4567@gmail.com>
2024-09-23 02:53:49 +0500

    Authorization  Added

---
+++ Desktop/CRUD_APP/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace CRUD_APP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            textBox4 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            textBox3 = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            dataGridView1 = new System.Windows.Forms.DataGridView();
+            nam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            dept = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            cgpa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            sem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new System.Drawing.Point(31, 29);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(306, 233);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Registration";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new System.Drawing.Point(129, 167);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(158, 27);
+            textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(38, 170);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(70, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Semester";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(129, 123);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(158, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(38, 126);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(45, 20);
+            label3.TabIndex = 4;
+            label3.Text = "CGPA";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(129, 82);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(158, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(34, 89);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(89, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Department";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(129, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(158, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(34, 39);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(49, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Name";
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(60, 302);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Insert";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(175, 302);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(285, 302);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(94, 29);
+            button3.TabIndex = 3;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { nam, dept, cgpa, sem });
+            dataGridView1.Location = new System.Drawing.Point(392, 29);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new System.Drawing.Size(553, 302);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // nam
+            // 
+            nam.HeaderText = "Name";
+            nam.MinimumWidth = 6;
+            nam.Name = "nam";
+            nam.Width = 125;
+            // 
+            // dept
+            // 
+            dept.HeaderText = "Department";
+            dept.MinimumWidth = 6;
+            dept.Name = "dept";
+            dept.Width = 125;
+            // 
+            // cgpa
+            // 
+            cgpa.HeaderText = "CGPA";
+            cgpa.MinimumWidth = 6;
+            cgpa.Name = "cgpa";
+            cgpa.Width = 125;
+            // 
+            // sem
+            // 
+            sem.HeaderText = "Semester";
+            sem.MinimumWidth = 6;
+            sem.Name = "sem";
+            sem.Width = 125;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(993, 371);
+            Controls.Add(dataGridView1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nam;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dept;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cgpa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sem;
+    }
+}
+
---
+++ Desktop/CRUD_APP/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CRUD_APP
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\lenovo\Documents\Crud_App_Data.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
